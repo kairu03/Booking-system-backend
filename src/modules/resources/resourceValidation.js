ca17c3bd67267666b8000e52e6445907b8b1905a
kairufrancisco <kairufrancisco@gmail.com>
2026-03-20 21:06:09 +0800

    refactor: resource validation using reusable fields (v2)

---
+++ src/modules/resources/resourceValidation.js
@@ -0,0 +1,37 @@
+import Joi from "joi";
+import { objectId } from "../common/validators/objectId";
+import { resourceFields } from "../common/validators/resourceFields";
+
+// for req.params - GET ResourceById && PATCH && DELETE
+export const resourceParamsSchema = Joi.object({
+  resourceId: objectId.string().length(24).hex().required().messages({
+    'any.required': 'Resource Id is required',
+  })
+});
+
+
+// for req.body - POST Resource
+export const createResourceSchema = Joi.object({
+  ...resourceFields,
+  name: resourceFields.name.required().messages({
+    'any.required': 'Resource name is required'
+  }),
+  capacity: resourceFields.capacity.required().messages({
+    'any.required': 'Capacity is required',
+  }),
+  price: resourceFields.price.required.messages({
+    'any.required': 'Price is required',
+  }),
+  pricingType: resourceFields.pricingType.required.messages({
+    'any.required': 'Pricing type is required'
+  }),
+  description: resourceFields.description.optional().allow(''),
+  amenities: resourceFields.amenities.optional.allow(''),
+  images: resourceFields.images.optional().allow(''),
+});
+
+
+// for req.body PATCH Resource
+export const updateResourceSchema = Joi.object({
+  ...resourceFields,
+}).min(1) // at least 1 field must be sent to update

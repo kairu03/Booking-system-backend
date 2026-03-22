f117725fb1d5493df5dc76ddeb1a36874aa210e1
kairufrancisco <kairufrancisco@gmail.com>
2026-03-22 18:32:47 +0800

    refactor login and registration to use createLimiter helper

--- src/middlewares/ratelimit/authLimiter.js
+++ src/middlewares/ratelimit/authLimiter.js
@@ -1,48 +1,21 @@
-import { redis } from "./redis.js";
-import { asyncHandler } from "../../utils/asyncHandler.js";
+import { createLimiter } from "./createLimiter.js";
 
 
 // limits each user per network
-export const loginLimiter = asyncHandler(async (req, res, next) => {
-  const ip = req.ip; // IP of the client (network)
-  const userId = req.user.id; // user Id from auth middleware
-  const key = `booking:login:${userId}:${ip}`; // unique key per user + IP
-
-  //  contains the current number of login attempts for this IP
-  const requests = await redis.incr(key);
-
-  if (requests === 1) {
-    await redis.expire(key, 60 * 15); // 15mins
-  }
-
-  if (requests > 10) {
-    return res.status(429).json({
-      success: false,
-      message: 'Too many login attempts, try again later'
-    });
-  }
-
-  next();
-});
+export const loginLimiter = createLimiter(
+  `booking:login`,
+  10,
+  60 * 15,
+  'Too many login attempts, try again later',
+  { useUserId: true, useIp: true}, // track by userId + ip
+);
 
 
 // limits per network
-export const registerLimiter = asyncHandler(async (req, res, next) => {
-  const ip = req.ip; 
-  const key = `booking:register:${ip}`;
-
-  const requests = await redis.incr(key);
-
-  if (requests === 1) {
-    await redis.expire(key, 60 * 15);
-  }
-
-  if (requests > 5) {
-    return res.status(429).json({
-      success: false,
-      message: 'Too many register attempts, try again later'
-    });
-  }
-
-  next();
-});
+export const registerLimiter = createLimiter(
+  'booking:register',
+  10,
+  60 * 15,
+  'Too many register attempts, try again later',
+  { useIp: true }, // track by ip
+);

e2aa2c735582424d85710144455a466741e23e81
kairufrancisco <kairufrancisco@gmail.com>
2026-03-29 21:25:16 +0800

    Add winston logger and replace console logs

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,12 +1,16 @@
+import { logger } from "../config/logger.js";
 
 export const errorHandler = (err, req, res, next) => {
 
   const statusCode = err.statusCode || res.statusCode || 500;
 
+  // log error to winston 
+  logger.error(`${err.name}: ${err.message}\n${err.stack}`);
+
   res.status(statusCode).json({
     success: false,
     message: err.message || 'Server error',
-    
+
     stack: process.env.NODE_ENV === 'development' ? err.stack : null
   });
 };
